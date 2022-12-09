65c8d20ad1f5dfea6266af589dd465956079c307
Ben Meadors <benmmeadors@gmail.com>
2022-12-09 17:39:23 -0600

    Thing

--- Meshtastic.Cli/Program.cs
+++ Meshtastic.Cli/Program.cs
@@ -4,6 +4,13 @@ using Meshtastic.Cli.Handlers;
 var portOption = new Option<string>(name: "--port", description: "Target serial port for meshtastic device");
 var hostOption = new Option<string>(name: "--host", description: "Target host ip or name for meshtastic device");
 
+var setOption = new Option<string>(name: "--save", "Set a config or module-config");
+// meshtastic get --pref mqtt.enable=true --pref mqtt.host=mqtt.meshtastic.org
+// meshtastic set --pref mqtt.enable=true --pref mqtt.host=mqtt.meshtastic.org
+// meshtastic set -p mqtt.enable=true -p mqtt.host=mqtt.meshtastic.org
+
+setOption.Add(new CompletionItem(label: "todo"));
+
 var monitorCommand = new Command("monitor", "Serial monitor for meshtastic devices");
 monitorCommand.SetHandler(MonitorHandler.Handle,
     new ConnectionBinder(portOption, hostOption), 
