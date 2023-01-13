1946307ab8d715eeccec50b1853deb89675d435c
Ben Meadors <benmmeadors@gmail.com>
2023-01-13 16:16:27 -0600

    More tests

---
+++ Meshtastic.Test/Data/TraceRouteMessageFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Meshtastic.Protobufs;
+using static Meshtastic.Protobufs.Config.Types;
+
+namespace Meshtastic.Test.Data;
+
+[TestFixture]
+public class TraceRouteMessageFactoryTests
+{
+    private Fixture fixture;
+    private DeviceStateContainer deviceStateContainer;
+    private TraceRouteMessageFactory factory;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixture = new Fixture();
+        deviceStateContainer = new DeviceStateContainer();
+        deviceStateContainer.MyNodeInfo.MyNodeNum = 100;
+        deviceStateContainer.LocalConfig = new LocalConfig
+        {
+            Lora = new LoRaConfig()
+            {
+                HopLimit = 3,
+            }
+        };
+        factory = new TraceRouteMessageFactory(deviceStateContainer);
+    }
+
+    [Test]
+    public void CreateRouteDiscoveryPacket_Should_ReturnValidAdminMessage()
+    {
+        factory = new TraceRouteMessageFactory(deviceStateContainer, 100);
+        var result = factory.CreateRouteDiscoveryPacket();
+        result.Decoded.Portnum.Should().Be(PortNum.TracerouteApp);
+    }
+
+    [Test]
+    public void CreateRouteDiscoveryPacket_Should_ThrowNotNullException()
+    {
+        factory = new TraceRouteMessageFactory(deviceStateContainer);
+        var action = () => factory.CreateRouteDiscoveryPacket();
+        action.Should().Throw<InvalidOperationException>();
+    }
+}
---
+++ Meshtastic.Test/Data/WaypointMessageFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Meshtastic.Protobufs;
+using static Meshtastic.Protobufs.Config.Types;
+
+namespace Meshtastic.Test.Data;
+
+[TestFixture]
+public class WaypointMessageFactoryTests
+{
+    private Fixture fixture;
+    private DeviceStateContainer deviceStateContainer;
+    private WaypointMessageFactory factory;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixture = new Fixture();
+        deviceStateContainer = new DeviceStateContainer();
+        deviceStateContainer.MyNodeInfo.MyNodeNum = 100;
+        deviceStateContainer.LocalConfig = new LocalConfig
+        {
+            Lora = new LoRaConfig()
+            {
+                HopLimit = 3,
+            }
+        };
+        factory = new WaypointMessageFactory(deviceStateContainer);
+    }
+
+    [Test]
+    public void CreateWaypointPacket_Should_ReturnValidAdminMessage()
+    {
+        var result = factory.CreateWaypointPacket(new Waypoint());
+        result.Decoded.Portnum.Should().Be(PortNum.WaypointApp);
+    }
+}
