ee8886b3bcdf90406786ad1ba33d7f720fae9917
Ben Meadors <benmmeadors@gmail.com>
2023-01-14 10:06:29 -0600

    Last reflection extensions tests

--- Meshtastic.Test/Extensions/ReflectionExtensionsTests.cs
+++ Meshtastic.Test/Extensions/ReflectionExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Meshtastic.Cli.Extensions;
 using Meshtastic.Protobufs;
+using static Meshtastic.Protobufs.Config.Types;
 
 namespace Meshtastic.Test.Extensions
 {
@@ -27,6 +28,21 @@ namespace Meshtastic.Test.Extensions
         {
             var list = new LocalModuleConfig().GetSettingsOptions();
             list.Should().Contain("Mqtt.Address");
+        }
+
+        [Test]
+        public void GetProperties_Should_ReturnPropertyOnConfigSection()
+        {
+            var list = new LoRaConfig().GetProperties();
+            list.Should().Contain(p => p.Name == "TxPower");
+        }
+
+        [Test]
+        public void GetSettingValue_Should_ReturnPropertyValueOnConfigSection()
+        {
+            var lora = new LoRaConfig() { TxPower = 100 };
+            var property = lora.GetProperties().First(p => p.Name == "TxPower");
+            property.GetSettingValue(lora).Should().BeEquivalentTo(lora.TxPower.ToString());
         }
     }
 }
