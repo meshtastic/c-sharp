72c499b3c485b8116d2ea9fdcc5bb44f175a6159
Ben Meadors <benmmeadors@gmail.com>
2023-01-17 09:28:51 -0600

    Protobufs

--- scripts/regen-protos.bat
+++ scripts/regen-protos.bat
@@ -1,1 +1,1 @@
-%USERPROFILE%/.nuget/packages/google.protobuf.tools/3.21.12/tools/windows_x64/protoc.exe -I=protobufs --csharp_out=./Meshtastic/Generated --csharp_opt=base_namespace=Meshtastic.Protobufs ./protobufs/*.proto
+%USERPROFILE%/.nuget/packages/google.protobuf.tools/3.21.12/tools/windows_x64/protoc.exe -I=protobufs --csharp_out=./Meshtastic/Generated --csharp_opt=base_namespace=Meshtastic.Protobufs ./protobufs/meshtastic/*.proto
