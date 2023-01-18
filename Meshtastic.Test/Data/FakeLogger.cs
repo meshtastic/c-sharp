e399b1b6b7fe4a1095842249d41b09a78bbccbd8
Ben Meadors <benmmeadors@gmail.com>
2023-01-17 18:46:38 -0600

    Tests

---
+++ Meshtastic.Test/Data/FakeLogger.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Meshtastic.Test.Data;
+
+public class FakeLogger : ILogger
+{
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+    }
+}
--- Meshtastic.Test/Data/FromDeviceMessageTests.cs
+++ Meshtastic.Test/Data/FromDeviceMessageTests.cs
@@ -1,24 +1,45 @@
+using AutoFixture;
 using Google.Protobuf;
-using Microsoft.Extensions.Logging;
+using Meshtastic.Extensions;
+using Meshtastic.Protobufs;
 
 namespace Meshtastic.Test.Data;
-
 public class FromDeviceMessageTests
 {
+    private FromDeviceMessage fromDeviceMessage;
+
+
     [SetUp]
     public void Setup()
     {
+        fromDeviceMessage = new FromDeviceMessage(new FakeLogger());
     }
 
     [Test]
     public void FromDeviceMessage_SwallowsException_Given_BadPayload()
     {
-        var fixture = new Fixture();
         var action = () =>
         {
-            var derp = new FromDeviceMessage(fixture.Create<ILogger>());
-            derp.ParsedFromRadio(BitConverter.GetBytes(123456));
+            fromDeviceMessage.ParsedFromRadio(BitConverter.GetBytes(123456));
         };
         action.Should().NotThrow<InvalidProtocolBufferException>();
+    }
+
+    [Test]
+    public void FromDeviceMessage_GivesResult_Given_ValidFromRadioPayload()
+    {
+        var fromRadio = new FromRadio()
+        {
+            Packet = new MeshPacket()
+            {
+                Decoded = new Protobufs.Data()
+                {
+                    Portnum = PortNum.AdminApp,
+                    Payload = new AdminMessage() { BeginEditSettings = true }.ToByteString(),
+                }
+            }
+        };
+        var result = fromDeviceMessage.ParsedFromRadio(fromRadio.ToByteArray());
+        result!.GetMessage<AdminMessage>()!.BeginEditSettings.Should().BeTrue();
     }
 }
