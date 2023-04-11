85a74c6de51106c6361ea9ac1b647da38be7ab9c
Ben Meadors <benmmeadors@gmail.com>
2023-04-11 10:00:08 -0500

    Putzing with TAK

--- Meshtastic.Cli/CommandHandlers/LiveCommandHandler.cs
+++ Meshtastic.Cli/CommandHandlers/LiveCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Meshtastic.Data;
+﻿using System.Net.Sockets;
+using Meshtastic.Data;
 using Meshtastic.Data.MessageFactories;
 using Meshtastic.Display;
 using Meshtastic.Protobufs;
@@ -7,10 +8,18 @@ namespace Meshtastic.Cli.CommandHandlers;
 
 public class LiveCommandHandler : DeviceCommandHandler
 {
+    private readonly TcpClient client = new();
+    private NetworkStream? networkStream;
     public LiveCommandHandler(DeviceConnectionContext context, CommandContext commandContext) : base(context, commandContext) { }
 
     public async Task<DeviceStateContainer> Handle()
     {
+        await client.ConnectAsync("192.168.2.23", 8087);
+        networkStream = client.GetStream();
+        var buffer = new byte[networkStream.Length];
+        await networkStream.ReadAsync(buffer);
+        var message = dpp.cot.Message.Parse(buffer, 0, buffer.Length);
+        AnsiConsole.WriteLine(message.ToXmlString());
         var wantConfig = new ToRadioMessageFactory().CreateWantConfigMessage();
         var container = await Connection.WriteToRadio(wantConfig, CompleteOnConfigReceived);
         Connection.Disconnect();
