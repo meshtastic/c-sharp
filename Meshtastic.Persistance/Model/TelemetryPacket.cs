483d019b954e1680898f7908119839a251e816f6
Ben Meadors <benmmeadors@gmail.com>
2024-01-11 10:38:13 -0600

    Remove service and persistence files

--- Meshtastic.Persistance/Model/TelemetryPacket.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Meshtastic.Persistance.Model;
-
-public class TelemetryPacket : MeshtasticPacket
-{
-    [Key]
-    public uint TelemetryId { get; set; }
-}
--- Meshtastic.Persistance/Model/WaypointPacket.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Meshtastic.Persistance.Model;
-
-public class WaypointPacket : MeshtasticPacket
-{
-    [Key]
-    public uint WaypointId { get; set; }
-}
