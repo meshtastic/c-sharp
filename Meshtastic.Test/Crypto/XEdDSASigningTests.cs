f3f475d17967701b38d5b510571237b74803445a
Łukasz Kosson <git@lukasz.kosson.net>
2026-07-08 15:39:47 +0200

    Deterministic test cases for positive/negative scalar sign during public key generation

--- Meshtastic.Test/Crypto/XEdDSASigningTests.cs
+++ Meshtastic.Test/Crypto/XEdDSASigningTests.cs
@@ -53,17 +53,8 @@ public class XEdDSASigningTests
     [Test]
     public void ConvertX25519PublicKeyToEd25519_Should_EqualToGenerateEdDSAKeysFromX25519_Positive()
     {
-        byte[] x25519PrivateKey;
-        byte[] x25519PublicKey;
-
-        // Generate X25519 keypairs until one producing positive Ed25519 is found
-        while (true)
-        {
-            (x25519PrivateKey, x25519PublicKey) = PKIEncryption.GenerateKeyPair();
-            var edPublic = XEdDSASigning.ConvertX25519PublicKeyToEd25519(x25519PublicKey, forcePositive: false);
-            if ((edPublic[31] & 0x80) == 0) break;
-        }
-
+        var x25519PrivateKey = Convert.FromBase64String("+ATTWKM68dlArUzWrXWlQm45Gi3ZYrOdDt2z5VT5+2o=");
+        var x25519PublicKey = PKIEncryption.GetPublicKeyFromPrivateKey(x25519PrivateKey);
         var edPublicKeyFromPublic = XEdDSASigning.ConvertX25519PublicKeyToEd25519(x25519PublicKey);
         var (_, edPublicKeyFromPrivate) = XEdDSASigning.GenerateEdDSAKeysFromX25519(x25519PrivateKey);
         Assert.That(edPublicKeyFromPrivate, Is.EqualTo(edPublicKeyFromPublic));
@@ -72,17 +63,8 @@ public class XEdDSASigningTests
     [Test]
     public void ConvertX25519PublicKeyToEd25519_Should_EqualToGenerateEdDSAKeysFromX25519_Negative()
     {
-        byte[] x25519PrivateKey;
-        byte[] x25519PublicKey;
-
-        // Generate X25519 keypairs until one producing negative Ed25519 is found
-        while (true)
-        {
-            (x25519PrivateKey, x25519PublicKey) = PKIEncryption.GenerateKeyPair();
-            var edPublic = XEdDSASigning.ConvertX25519PublicKeyToEd25519(x25519PublicKey, forcePositive: false);
-            if ((edPublic[31] & 0x80) != 0) break;
-        }
-
+        var x25519PrivateKey = Convert.FromBase64String("wJNheemu5n2oPgpu0BpEdomsPlChBSM8gAO7RRWkT2w=");
+        var x25519PublicKey = PKIEncryption.GetPublicKeyFromPrivateKey(x25519PrivateKey);
         var edPublicKeyFromPublic = XEdDSASigning.ConvertX25519PublicKeyToEd25519(x25519PublicKey);
         var (_, edPublicKeyFromPrivate) = XEdDSASigning.GenerateEdDSAKeysFromX25519(x25519PrivateKey);
         Assert.That(edPublicKeyFromPrivate, Is.EqualTo(edPublicKeyFromPublic));
